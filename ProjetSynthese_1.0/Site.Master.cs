2d19d7c01478f6418c8b8595633c56902c47a45e
Dan-Ghenadie Roman <danghenadieroman@gmail.com>
2017-02-08 20:53:11 -0500

    corection site.master

--- ProjetSynthese_1.0/Site.Master.cs
+++ ProjetSynthese_1.0/Site.Master.cs
@@ -11,17 +11,11 @@ namespace ProjetSynthese_1._0
     public partial class SiteMaster : MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
+        {    
             if (!IsPostBack)
             {
                 GestionUtilisateur.SetupEspaceTravail(this);
             }
-        }
-
-        protected void btnDeconnexion_Click(object sender, EventArgs e)
-        {
-            //ici il faut detruire session utilisateur etc.
-            Server.Transfer("~/Login.aspx");
         }
     }
 }
