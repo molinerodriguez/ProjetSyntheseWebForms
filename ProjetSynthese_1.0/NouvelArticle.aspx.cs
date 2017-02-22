60fddc4ebeba4bafa877fe37a550f9f738c5ce35
Dan-Ghenadie Roman <danghenadieroman@gmail.com>
2017-02-21 20:01:41 -0500

    amelioration interfaces

--- ProjetSynthese_1.0/NouvelArticle.aspx.cs
+++ ProjetSynthese_1.0/NouvelArticle.aspx.cs
@@ -13,6 +13,14 @@ namespace ProjetSynthese_1._0
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            genererDropDownListCategorie();
+
+        }
+
+        private void genererDropDownListCategorie()
+        {
+            ddlCategorieNouvelArticle.Items.Add("Fruits");
+            ddlCategorieNouvelArticle.Items.Add("Legumes");
         }
 
         protected void btnEnregistrer_Click(object sender, EventArgs e)
@@ -24,42 +32,13 @@ namespace ProjetSynthese_1._0
         }
 
         #region proprietes
-        public TextBox TxtNum
-        {
-            get { return this.txtNum; }
-        }
-
-        public TextBox TxtNom
-        {
-            get { return this.txtNom; }
-        }
-
-        public TextBox TxtDescription
-        {
-            get { return this.txtDescription; }
-        }
-
-        public TextBox TxtCategorie
-        {
-            get { return this.txtCategorie; }
-        }
-
-        public TextBox TxtPxAchat
-        {
-            get { return this.txtPxAchat; }
-        }
-
-        public TextBox TxtPxVente
-        {
-            get { return this.txtPxVente; }
-        }
-
-        public Label LblResultatNouvelArticle
-        { get
-            {
-                return this.lblResultatNouvelArticle;
-            }
-        }
+        public TextBox TxtNum { get { return this.txtNum; } }
+        public TextBox TxtNom { get { return this.txtNom; } }
+        public TextBox TxtDescription { get { return this.txtDescription; } }
+        public DropDownList DDLCategorieNouvelArticle { get { return this.ddlCategorieNouvelArticle; } }
+        public TextBox TxtPxAchat { get { return this.txtPxAchat; } }
+        public TextBox TxtPxVente { get { return this.txtPxVente; } }
+        public Label LblResultatNouvelArticle { get { return this.lblResultatNouvelArticle; } }
         #endregion
     }
 }
--- ProjetSynthese_1.0/NouvelArticle.aspx.designer.cs
+++ ProjetSynthese_1.0/NouvelArticle.aspx.designer.cs
@@ -94,22 +94,13 @@ namespace ProjetSynthese_1._0 {
         protected global::System.Web.UI.WebControls.Label lblCategorie;
         
         /// <summary>
-        /// txtCategorie control.
+        /// ddlCategorieNouvelArticle control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtCategorie;
-        
-        /// <summary>
-        /// RequiredFieldValidator3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+        protected global::System.Web.UI.WebControls.DropDownList ddlCategorieNouvelArticle;
         
         /// <summary>
         /// lblPxAchat control.
